c5aaccdb5048430d14e77fe773c3cdcadd016aa8
LucasSusin <lssusin@gmail.com>
2020-10-26 17:33:10 -0300

    Classe acidentes e submenus

--- src/Menu.java
+++ src/Menu.java
@@ -28,10 +28,10 @@ public class Menu {
 					System.out.println("Sistema de consultas sobre os acidentes de trânsito de Porto Alegre");
 					break;
 				case 2:
-
+					menuNavegar();
 					break;
 				case 3:
-
+					menuConsultas();
 					break;
 				case 4:
 
@@ -43,7 +43,7 @@ public class Menu {
 				default:
 					System.out.println("Esta opção não existe! Digite novamente.");
 			}
-		} while (opcao != 4);
+		} while (opcao != 5);
 	}
 
 	/**
@@ -56,11 +56,11 @@ public class Menu {
 		System.out
 				.println("                  |     1 - Sobre                                                         |");
 		System.out
-				.println("                  |     2 - ?                                                             |");
+				.println("                  |     2 - Navegar                                                       |");
 		System.out
-				.println("                  |     3 - ?                                                             |");
+				.println("                  |     3 - Consultas                                                     |");
 		System.out
-				.println("                  |     4 - ?                                                             |");
+				.println("                  |     4 - Total de acidentes envolvendo moto                            |");
 		System.out
 				.println("                  |     5 - Sair                                                          |");
 		System.out.println(
@@ -87,4 +87,78 @@ public class Menu {
 		}
 		return numero;
 	}
+
+	/**
+	 * Submenu para consultas
+	 */
+	private void menuConsultas() {
+		System.out.println("\n                                         ### Você selecionou: Consultas! ###");
+		System.out
+				.println("                   ========================================================================");
+		System.out
+				.println("                  |     1 - RUA/AVENIDA/TRAVESSIA com mais acidentes                      |");
+		System.out
+				.println("                  |     2 - Dia da semana em X local que mais ocorreram acidentes         |");
+		System.out
+				.println("                  |     3 - Voltar para o menu principal                                  |");
+		System.out.println(
+				"                   ========================================================================\n");
+		
+		int opcao = -1;
+
+		do {
+			opcao = leInteiro();
+			System.out.println("*************************************************");
+			switch (opcao) {
+				case 1:
+					// TODO: método para mostrar RUA/AVENIDA/TRAVESSIA com mais acidentes  
+				case 2:
+					// TODO: método para mostrar dia da semana em X local que mais ocorreram acidentes   
+					break;
+				case 3:
+					System.out.println("Voltando...");
+					break;
+				default:
+					System.out.println("Esta opção não existe! Digite novamente.");
+			}
+		} while (opcao != 3);
+	}
+
+	/**
+	 * Submenu para navegar
+	 */
+	private void menuNavegar() {
+		System.out.println("\n                                         ### Você selecionou: Navegar! ###");
+		System.out
+				.println("                   ========================================================================");
+		System.out
+				.println("                  |     1 - Avançar                                                       |");
+		System.out
+				.println("                  |     2 - Retroceder                                                    |");
+		System.out
+				.println("                  |     3 - Voltar para o menu principal                                  |");
+		System.out.println(
+				"                   ========================================================================\n");
+		
+		int opcao = -1;
+
+		do {
+			opcao = leInteiro();
+			System.out.println("*************************************************");
+			switch (opcao) {
+				case 1:
+					// TODO: avancar um na lista 
+				case 2:
+					// TODO: retroceder um na lista 
+					break;
+				case 3:
+					System.out.println("Voltando...");
+					break;
+				default:
+					System.out.println("Esta opção não existe! Digite novamente.");
+			}
+		} while (opcao != 3);
+	}
+
 }
+
